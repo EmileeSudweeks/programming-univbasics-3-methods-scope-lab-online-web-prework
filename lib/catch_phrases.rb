2acb1754572c85e63b941d4db3ecda9c232e2a5b
Emilee Sudweeks <emilee.sudweeks@wework.com>
2019-11-04 19:24:20 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,6 +3,9 @@ def link
   puts phrase
 end
 
-def toadstool
-  puts status
+describe "any_phrase" do
+  it "takes in an argument and puts out the catch phrase" do
+    phrase = "Do A Barrel Roll!"
+    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
+  end
 end
