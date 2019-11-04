546674e137a1602577203fa2d184747ebd729f6f
Emilee Sudweeks <emilee.sudweeks@wework.com>
2019-11-04 19:25:09 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,10 +2,3 @@ def link
   status = "It's Dangerous To Go Alone! Take This."
   puts phrase
 end
-
-describe "any_phrase" do
-  it "takes in an argument and puts out the catch phrase" do
-    phrase = "Do A Barrel Roll!"
-    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
-  end
-end
